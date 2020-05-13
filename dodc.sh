7cfde188b0409dfd5fe4eb0a6b258518bdc94dc0
Nicholas P. Cole <nicholas@nicholaspcole.com>
2020-05-12 23:38:10 -0400

    Update for currently available images, regions, and sizes

--- dodc.sh
+++ dodc.sh
@@ -75,27 +75,58 @@ function choose_size() {
       s-20vcpu-96gb "Standard Droplet, 1920 GB SSD, \$480/mo." off \
       s-24vcpu-128gb "Standard Droplet, 2560 GB SSD, \$640/mo." off \
       s-32vcpu-192gb "Standard Droplet, 3840 GB SSD, \$960/mo." off \
-      c-1vcpu-2gb "Optimized Droplet, \$20/mo." off \
-      c-2 "Optimized Droplet, \$40/mo." off \
-      c-4 "Optimized Droplet, \$80/mo." off \
-      c-8 "Optimized Droplet, \$160/mo." off \
-      c-16 "Optimized Droplet, \$320/mo." off \
-      c-32 "Optimized Droplet, \$640/mo." off \
-      c-48 "Optimized Droplet, \$960/mo." off \
-      512mb "1st generation Droplet, \$5/mo." off \
-      1gb "1st generation Droplet, \$10/mo." off \
-      2gb "1st generation Droplet, \$20/mo." off \
-      4gb "1st generation Droplet, \$40/mo." off \
-      8gb "1st generation Droplet, \$80/mo." off \
-      16gb "1st generation Droplet, \$160/mo." off \
-      32gb "1st generation Droplet, \$320/mo." off \
-      48gb "1st generation Droplet, \$480/mo." off \
-      64gb "1st generation Droplet, \$640/mo." off \
-      m-16gb "High Memory Droplet, \$120/mo." off \
-      m-32gb "High Memory Droplet, \$240/mo." off \
-      m-64gb "High Memory Droplet, \$480/mo." off \
-      m-128gb "High Memory Droplet, \$960/mo." off \
-      m-224gb "High Memory Droplet, \$1680/mo." off 2>${DIALOG_RESPONSE_TMP}
+      g-2vcpu-8gb "General Purpose, 25 GB SSD, \$60/mo." off \
+      g-4vcpu-16gb "General Purpose, 50 GB SSD, \$120/mo." off \
+      g-8vcpu-32gb "General Purpose, 100 GB SSD, \$240/mo." off \
+      g-16vcpu-64gb "General Purpose, 200 GB SSD, \$480/mo." off \
+      g-32vcpu-128gb "General Purpose, 400 GB SSD, \$960/mo." off \
+      g-40vcpu-160gb "General Purpose, 500 GB SSD, \$1200/mo." off \
+      gd-2vcpu-8gb "General Purpose 2x SSD, 50 GB SSD, \$65/mo." off \
+      gd-4vcpu-16gb "General Purpose 2x SSD, 100 GB SSD, \$130/mo." off \
+      gd-8vcpu-32gb "General Purpose 2x SSD, 200 GB SSD, \$260/mo." off \
+      gd-16vcpu-64gb "General Purpose 2x SSD, 400 GB SSD, \$520/mo." off \
+      gd-32vcpu-128gb "General Purpose 2x SSD, 800 GB SSD, \$1040/mo." off \
+      gd-40vcpu-160gb "General Purpose 2x SSD, 1000 GB SSD, \$1300/mo." off \
+      c-1vcpu-2gb "CPU-Optimized, 25 GB SSD, \$20/mo." off \
+      c-2 "CPU-Optimized, 25 GB SSD, \$40/mo." off \
+      c-4 "CPU-Optimized, 50 GB SSD, \$80/mo." off \
+      c-8 "CPU-Optimized, 100 GB SSD, \$160/mo." off \
+      c-16 "CPU-Optimized, 200 GB SSD, \$320/mo." off \
+      c-32 "CPU-Optimized, 400 GB SSD, \$640/mo." off \
+      c-48 "CPU-Optimized, 20 GB SSD, \$960/mo." off \
+      m-2vcpu-16gb "Memory-Optimized, 50 GB SSD, \$90/mo." off \
+      m-4vcpu-32gb "Memory-Optimized, 100 GB SSD, \$180/mo." off \
+      m-8vcpu-64gb "Memory-Optimized, 200 GB SSD, \$360/mo." off \
+      m-16vcpu-128gb "Memory-Optimized, 400 GB SSD, \$720/mo." off \
+      m-24vcpu-192gb "Memory-Optimized, 600 GB SSD, \$1080/mo." off \
+      m-32vcpu-256gb "Memory-Optimized, 800 GB SSD, \$1440/mo." off \
+      m3-2vcpu-16gb "Memory-Optimized 3x SSD, 150 GB SSD, \$110/mo." off \
+      m3-4vcpu-32gb "Memory-Optimized 3x SSD, 300 GB SSD, \$220/mo." off \
+      m3-8vcpu-64gb "Memory-Optimized 3x SSD, 600 GB SSD, \$440/mo." off \
+      m3-16vcpu-128gb "Memory-Optimized 3x SSD, 1200 GB SSD, \$880/mo." off \
+      m3-24vcpu-192gb "Memory-Optimized 3x SSD, 1800 GB SSD, \$1320/mo." off \
+      m3-32vcpu-256gb "Memory-Optimized 3x SSD, 2400 GB SSD, \$1760/mo." off \
+      m6-2vcpu-16gb "Memory-Optimized 6x SSD, 300 GB SSD, \$140/mo." off \
+      m6-4vcpu-32gb "Memory-Optimized 6x SSD, 600 GB SSD, \$280/mo." off \
+      m6-8vcpu-64gb "Memory-Optimized 6x SSD, 1200 GB SSD, \$560/mo." off \
+      m6-16vcpu-128gb "Memory-Optimized 6x SSD, 2400 GB SSD, \$1120/mo." off \
+      m6-24vcpu-192gb "Memory-Optimized 6x SSD, 3600 GB SSD, \$1680/mo." off \
+      m6-32vcpu-256gb "Memory-Optimized 6x SSD, 4800 GB SSD, \$2240/mo." off \
+      512mb "(legacy) Standard, 20 GB SSD, \$5/mo." off \
+      1gb "(legacy) Standard, 30 GB SSD, \$10/mo." off \
+      2gb "(legacy) Standard, 40 GB SSD, \$20/mo." off \
+      4gb "(legacy) Standard, 60 GB SSD, \$40/mo." off \
+      8gb "(legacy) Standard, 80 GB SSD, \$80/mo." off \
+      16gb "(legacy) Standard, 160 GB SSD, \$160/mo." off \
+      32gb "(legacy) Standard, 320 GB SSD, \$320/mo." off \
+      48gb "(legacy) Standard, 480 GB SSD, \$480/mo." off \
+      64gb "(legacy) Standard, 640 GB SSD, \$640/mo." off \
+      m-1vcpu-8gb "(legacy) High Memory, 40 GB SSD, \$40/mo." off \
+      m-16gb "(legacy) High Memory, 60 GB SSD, \$75/mo." off \
+      m-32gb "(legacy) High Memory, 90 GB SSD, \$150/mo." off \
+      m-64gb "(legacy) High Memory, 200 GB SSD, \$300/mo." off \
+      m-128gb "(legacy) High Memory, 340 GB SSD, \$600/mo." off \
+      m-224gb "(legacy) High Memory, 500 GB SSD, \$1100/mo." off 2>${DIALOG_RESPONSE_TMP}
     local dialog_exit_code=$?
     local dialog_input=$(cat ${DIALOG_RESPONSE_TMP})
 
@@ -117,6 +148,7 @@ function choose_region() {
       nyc3 "New York 3" off \
       sfo1 "San Francisco 1" off \
       sfo2 "San Francisco 2" off \
+      sfo3 "San Francisco 3" off \
       sgp1 "Singapore 1" off \
       tor1 "Toronto 1" off 2>${DIALOG_RESPONSE_TMP}
     local dialog_exit_code=$?
